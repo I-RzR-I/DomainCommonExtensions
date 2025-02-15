d3a24065bfb648ee3484feb4a79ae602e78956fe
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-02-15 13:17:35 +0200

    AES encrypt/decrypt refactor.
    Add default parameters for AES encrypt/decrypt (key size => 256 and block size => 128).
    Adjust encrypt/decrypt key using (allow use long key).
    Set padding to PKCS7, set mode to CBC.

--- src/tests/DataTypeTests/CryptoExtensionsTests.cs
+++ src/tests/DataTypeTests/CryptoExtensionsTests.cs
@@ -16,6 +16,7 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Security.Cryptography;
 using DomainCommonExtensions.CommonExtensions;
 
 namespace DataTypeTests
@@ -23,6 +24,28 @@ namespace DataTypeTests
     [TestClass]
     public class CryptoExtensionsTests
     {
+        private static string RandomString(int size)
+        {
+            const string alphabet = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var random = new Random();
+            var chars = new char[size];
+            for (var i = 0; i < size; i++)
+            {
+                chars[i] = alphabet[random.Next(alphabet.Length)];
+            }
+
+            return new string(chars);
+        }
+
+        private static string GenerateXBitKey(int keySize)
+        {
+            using var aesAlg = Aes.Create();
+            aesAlg.KeySize = keySize;
+            aesAlg.GenerateKey();
+
+            return Convert.ToBase64String(aesAlg.Key);
+        }
+
         [TestMethod]
         public void AesCrypt_Decrypt_iv_16_Test()
         {
@@ -59,18 +82,61 @@ namespace DataTypeTests
             Assert.IsNotNull(decryptedText);
             Assert.AreEqual(message, decryptedText);
         }
-        
+
         [TestMethod]
-        public void RsaCrypt_Decrypt_Test()
+        public void AesCrypt_Decrypt_iv_16_2_Test_2()
         {
             var message = "Here is your current message";
-            var key = "W+CLseRlv3I9dZmyaeDoeoKPFiFRWHkJkZOBOCPtx9c=";
+            var key = RandomString(24);
+            var iv = new byte[16];
+            new Random().NextBytes(iv);
 
-            var encryptedText = message.EncryptWithRSA(key);
+            var encryptedText = message.AesEncryptString(key, iv);
 
             Assert.IsNotNull(encryptedText);
 
-            var decryptedText = encryptedText.DecryptWithRSA(key);
+            var decryptedText = encryptedText.AesDecryptString(key, iv);
+
+            Assert.IsNotNull(decryptedText);
+            Assert.AreEqual(message, decryptedText);
+        }
+
+        [TestMethod]
+        public void AesCrypt_Decrypt_iv_16_LongKey_Test()
+        {
+            var message = "Here is your current message";
+            var key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7pbDRvuqRL0EsKjYGqPZWu4VF";
+            var iv = new byte[16];
+            new Random().NextBytes(iv);
+
+            var encryptedText = message.AesEncryptString(key, iv, true);
+
+            Assert.IsNotNull(encryptedText);
+
+            var decryptedText = encryptedText.AesDecryptString(key, iv, true);
+
+            Assert.IsNotNull(decryptedText);
+            Assert.AreEqual(message, decryptedText);
+        }
+
+        [DataRow(128, 128, 16, false)]
+        [DataRow(128, 128, 16, false)]
+        [DataRow(192, 128, 16, true)]
+        [DataRow(192, 128, 16, false)]
+        [DataRow(256, 128, 16, true)]
+        [TestMethod]
+        public void AesCrypt_Decrypt_iv_16_LongKey_Test(int blockKeySize, int blockSize, int blockByteSize, bool useLongKey)
+        {
+            var message = "Here is your current message";
+            var key = GenerateXBitKey(blockKeySize);
+            var iv = new byte[blockByteSize];
+            new Random().NextBytes(iv);
+
+            var encryptedText = message.AesEncryptString(key, iv, useLongKey, blockKeySize, blockSize);
+
+            Assert.IsNotNull(encryptedText);
+
+            var decryptedText = encryptedText.AesDecryptString(key, iv, useLongKey, blockKeySize, blockSize);
 
             Assert.IsNotNull(decryptedText);
             Assert.AreEqual(message, decryptedText);
